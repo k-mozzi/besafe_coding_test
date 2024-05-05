dbcd42b010ee58ec9e6a8ffb726057ddec7bc076
k-mozzi <kimkm0405@gmail.com>
2024-05-05 16:44:46 +0900

    User 클래스

---
+++ src/main/java/besafe/codingTest/domain/user/User.java
@@ -0,0 +1,31 @@
+package besafe.codingTest.domain.user;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/*
+* `utType` '회원 종류',
+* `useYn` '탈퇴여부',
+* `lastRefTokenTime` '리프레시토큰정보',
+* `mDate` 수정일자',
+* `cDate` '생성일자',
+* */
+@Data
+public class User {
+    private int uId;
+    private String email;
+    private String password;
+    private String name;
+    private Integer utType;
+    private String useYn;
+    private Date lastRefTokenTime;
+    private Date mDate;
+    private Date cDate;
+    private String profile;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
