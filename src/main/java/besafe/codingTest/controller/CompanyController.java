af863f153031db6d449a6fa246136f53752e59a9
k-mozzi <kimkm0405@gmail.com>
2024-05-08 19:44:21 +0900

    CompanyController API docs 수정

--- src/main/java/besafe/codingTest/controller/CompanyController.java
+++ src/main/java/besafe/codingTest/controller/CompanyController.java
@@ -4,6 +4,7 @@ import besafe.codingTest.service.CompanyService;
 import lombok.Data;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,15 @@ import java.util.Map;
 
 @Data
 @RestController
+@RequestMapping("/homepage")
 public class CompanyController {
     private final CompanyService companyService;
 
-    @GetMapping("/homepage/general/main")
+    /*
+    * 최근 10일 동안 브랜딩 계약이 많은 상위 20개 업체 출력 메서드
+    * Postman Body에 offset 입력
+    * */
+    @GetMapping("/general/main")
     public List<Map<String, Object>> getTopRatedCompanies(
             @RequestParam(name = "offset", defaultValue = "0") int offset) {
         return companyService.findByNumOfSale(offset);
